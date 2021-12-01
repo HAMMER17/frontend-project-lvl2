b9b3839bdc93a363c19d86ba198f6f43cade3f10
HAMMER17 <plutalov70gmail.com>
2021-12-01 16:42:52 +0300

    test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,11 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2, { format }) => {
-        console.log(genDiff(filepath1, filepath2, format));
-    });
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, { format }) => {
+    console.log(genDiff(filepath1, filepath2, format));
+  });
 program.parse();
--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filen
 const extractFormat = (filename) => path.extname(filename).slice(1);
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-    const file1format = extractFormat(filepath1);
-    const file2format = extractFormat(filepath2);
-    const fileContent1 = readFile(filepath1);
-    const fileContent2 = readFile(filepath2);
-    const data1 = parse(file1format, fileContent1);
-    const data2 = parse(file2format, fileContent2);
-    const innerTree = buildTree(data1, data2);
-    return format(innerTree, formatName);
+  const file1format = extractFormat(filepath1);
+  const file2format = extractFormat(filepath2);
+  const fileContent1 = readFile(filepath1);
+  const fileContent2 = readFile(filepath2);
+  const data1 = parse(file1format, fileContent1);
+  const data2 = parse(file2format, fileContent2);
+  const innerTree = buildTree(data1, data2);
+  return format(innerTree, formatName);
 };
 export default genDiff;
