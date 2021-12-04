027834bee0a04be6b18244d717c33cf052f13aaa
HAMMER17 <plutalov70gmail.com>
2021-12-04 15:13:12 +0300

    3

--- src/parsers.js
+++ src/parsers.js
@@ -3,10 +3,8 @@ import yaml from 'js-yaml';
 export default (format, data) => {
   switch (format) {
     case 'json':
-
       return JSON.parse(data);
     case 'yml':
-
       return yaml.load(data);
     default:
       throw new Error(`Формат не поддерживается: ${format}`);
